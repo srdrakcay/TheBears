22069d209629e39db92f8971b4ea77fe429250e3
srdrakcay <srdrakcay25@gmail.com>
2023-12-10 19:21:59 +0300

    start the learn ui end unit testing

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TheBears"
+include(":app")
+include(":feature:home")
+include(":feature:signin")
+include(":feature:signup")
+include(":feature:profile")
