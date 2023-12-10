25fa88fde1c9cf3fae9b5397b3dbdc0f91c5ad14
srdrakcay <srdrakcay25@gmail.com>
2023-12-10 23:00:02 +0300

    add create new account and signIn method

--- app/src/main/java/com/serdar/thebears/MainActivity.kt
+++ app/src/main/java/com/serdar/thebears/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.serdar.thebears
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/serdar/thebears/TheBearsApp.kt
@@ -0,0 +1,8 @@
+package com.serdar.thebears
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TheBearsApp:Application() {
+}
