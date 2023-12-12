33769200064bc987c9ce5dd757956234a080dbfb
srdrakcay <srdrakcay25@gmail.com>
2023-12-12 14:01:50 +0300

    all done on this page without test

--- feature/signin/src/main/java/com/serdar/signin/ui/SignInFragment.kt
+++ feature/signin/src/main/java/com/serdar/signin/ui/SignInFragment.kt
@@ -1,6 +1,7 @@
 package com.serdar.signin.ui
 
-import android.util.Log
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.serdar.common.base.BaseFragment
@@ -10,35 +11,66 @@ import com.serdar.common.validation.validationRule
 import com.serdar.navigation.R
 import com.serdar.signin.databinding.FragmentSignInBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SignInFragment : BaseFragment<FragmentSignInBinding>(FragmentSignInBinding::inflate) {
     private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel by viewModels<SignInViewModel>()
+
     override fun observeUi() {
         super.observeUi()
         setCurrentUser()
-        binding.btnLogIn.setOnClickListener {
-
-            Log.e("TAG", "observeUi: ${validateRule()}", )
-        }
+        initObserveLogin()
+        login()
     }
 
     private fun setCurrentUser() {
         firebaseAuth = FirebaseAuth.getInstance()
         if (firebaseAuth.currentUser != null) {
-            //findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
-        } else {
-           // findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
-
+            findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
         }
     }
-    private fun validateRule():Boolean{
-        val email=binding.editTxtEmail.validationRule(
-           rules = listOf(EmptyTextRule())
+
+    private fun validateRule(): Boolean {
+        val email = binding.editTxtEmail.validationRule(
+            rules = listOf(EmptyTextRule())
         )
-        val password=binding.editTxtPassword.validationRule(
-            rules = listOf(EmptyTextRule(),PasswordRule())
+        val password = binding.editTxtPassword.validationRule(
+            rules = listOf(EmptyTextRule(), PasswordRule())
         )
         return email && password
+    }
+
+    private fun initObserveLogin() {
+        viewModel.viewModelScope.launch {
+            viewModel.signInUiState.collect {
+                when (it) {
+                    is SignInUiState.Loading -> {
+
+                    }
+
+                    is SignInUiState.Error -> {
+
+                    }
+
+                    is SignInUiState.Success -> {
+                        findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun login() {
+        binding.btnLogIn.setOnClickListener {
+            if (validateRule()) {
+                val emailText = binding.editTxtEmail.text.toString()
+                val passwordText = binding.editTxtPassword.text.toString()
+                viewModel.signInWithFirebaseAuth(emailText, passwordText)
+            }
+        }
     }
 }
